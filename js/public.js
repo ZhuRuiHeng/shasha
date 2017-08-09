2380c63bfe3a44732fdcb083347622a93d312ef1
朱瑞恒 <18749830459@163.com>
2017-08-09 16:48:02 +0800

    修改上拉刷新下拉加载页面跳转

--- js/public.js
+++ js/public.js
@@ -2,5 +2,20 @@ var webRoot = "http://api.gdswww.com";
 var apiRoot = webRoot+"/salsa";
 var token = "7fc2ca72-7a94-4154-baf1-223741660d74";
 
+//页面跳转
+function openNewPage(id){
+	mui.openWindow({
+		id: id,
+		url: id,
+		show:{
+//			aniShow: 'fade-in-left',
+			duration: '300'			
+		},
+		waiting:{
+			autoShow:false
+		}
+	});
+}
+
 
 

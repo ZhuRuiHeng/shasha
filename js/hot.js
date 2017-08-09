a0b7fd9ec38d48bc04ae1ecb77fc073e609dd65c
朱瑞恒 <18749830459@163.com>
2017-08-09 19:05:05 +0800

    热门推荐注入交互原生方法

--- js/hot.js
+++ js/hot.js
@@ -17,7 +17,7 @@ xmlhttp.onreadystatechange=function(){
 		for(var i=0;i<data.length;i++){
     		//console.log(data[i].dityimg)
     		var num=i+1;
-       		var informStr="<div class=\"swiper-slide\" style=\"opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;\" onclick=\"shijian(this,"+data[i].dityprice+")\">"+
+       		var informStr="<div class=\"swiper-slide\" style=\"opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;\" onclick=\"shijian(this,"+data[i].id+")\">"+
 							"<img src=\""+data[i].dityimg+"\" class=\"main-img\">"+
 							"<p class=\"tent\"><span>"+data[i].dityname+"</span><span>￥<i>"+data[i].dityprice+"</i></span></p>"+
 						  "</div>";
@@ -30,3 +30,23 @@ xmlhttp.onreadystatechange=function(){
 xmlhttp.open("GET",apiRoot+"/commdity/getMore.do?token="+token,true);
 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xmlhttp.send();
+
+//交互js
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
+    if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.className = 'new';
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() { document.documentElement.removeChild(WVJBIframe) }, 0)
+}
+function shijian(obj,id) {
+    setupWebViewJavascriptBridge(function(bridge) {
+        bridge.callHandler('getHotImg', {'blogURL': id}, function(response) {
+
+        })
+    })
+}
